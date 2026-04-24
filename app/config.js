157c852caedce96f0da1511c4ef5ffcbb5838769
liuqiang <2465199797@qq.com>
2026-04-24 16:36:18 +0800

    优化 结构 和 歌单。（分割 18/23）
    按文件大小拆分自 713b64a。
    本组约 50.00 MiB，23 个文件。

---
+++ app/config.js
@@ -0,0 +1,19 @@
+export const CONFIG = {
+  UPDATE_INTERVAL: 500,
+  SCROLL_SPEED: 150,
+  TRANSITION: {
+    DELAY: 2500,
+    INTERVAL: 20,
+    REDUCE: 50
+  },
+  VOLUME: {
+    DEFAULT: 0.8,
+    MAX: 1
+  },
+  // 加载的歌词类型， 3 为文件形式 99 为纯音乐
+  LOAD_LYRICS_TYPE: {
+    TYPE_chunyinyue: 99,
+    TYPE_file: 3,
+  }
+
+};
