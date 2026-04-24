ca00b270bf3cbfb1acf47e1a88ac26af9460b20f
liuqiang <2465199797@qq.com>
2026-04-24 16:36:18 +0800

    优化 结构 和 歌单。（分割 2/23）
    按文件大小拆分自 713b64a。
    本组约 50.00 MiB，8 个文件。

---
+++ catalog/index.js
@@ -0,0 +1,19 @@
+import { lightMusicList } from './qingyinyue.js';
+import { healingList } from './shangganzhiyu.js';
+import { popClassicsList } from './liuxingjingdian.js';
+import { epicScoreList } from './shishipeiyue.js';
+import { rhythmList } from './jiezoulvdong.js';
+import { animeScreenList } from './dongmanyingshi.js';
+import { nostalgiaList } from './huaijiujinqv.js';
+import { folkList } from './minyao.js';
+
+export const MUSIC_LISTS = {
+  lightMusicList,
+  healingList,
+  popClassicsList,
+  epicScoreList,
+  rhythmList,
+  animeScreenList,
+  nostalgiaList,
+  folkList
+};
